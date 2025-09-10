a5f68f4a525ce0267d9146f50bbff74e06761756
ABCdario-00 <kevinsleao@gmail.com>
2025-09-10 13:54:48 -0300

    segundo commit, incompleto desculpa

--- main.py
+++ main.py
@@ -7,14 +7,48 @@ os.system("cls")
 pg.init()
 
 #Criando variaveis que armazenam o tamanho da nossa janela
-largura = 800
-altura = 600
+largura_J = 800
+altura_J = 600
+
+#Cores
+BRANCO = (255, 255, 255)
+VERMELHO = (255, 0, 0)
+AZUL = (0, 0, 255)
 
 #Cria a janela e armazena em uma variável
-tela = pg.display.set_mode((largura, altura))
+tela = pg.display.set_mode((largura_J, altura_J))
 
 #Definindo o nome da janela
 pg.display.set_caption("Joguin dos maia papai rs")
+
+#Carregando o nosso png de personagem
+try:
+    personagem_img = pg.image.load('bolso.png')
+#tratamento de erros
+except pg.error:
+    print(f"Não foi possível carregar imagem 'bolso.png' ")
+    print(f"Certifique-se de que o a imagem esteja na mesma página do projeto")
+
+    #Substituindo a imagem caso ela não seja encontrada
+    personagem_img = pg.Surface(50,50)
+    personagem_img.fill(AZUL)
+
+
+#Tamanho do personagem
+altura_personagem = 80
+largura_personagem = 80
+
+#Redimensionando personagem
+personagem_img_redimensionado = pg.transform.scale(personagem_img, (largura_personagem, altura_personagem))
+
+#Posição do personagem
+pos_personagem_x = 300
+pos_personagem_y = 400
+
+#Localização inicial
+pos_x = 50
+pos_y = 50
+
 
 #Criando loop de gameplay
 rodando = True
@@ -26,11 +60,19 @@ while rodando:
         if evento.type == pg.QUIT:
             rodando = False #Mudando o valor da variável de controle para acabar com o loop
             
-    # 2. Atualização do jogo (sem nada por enquanto)
-    
-    # 3. Desenhar na tela
+    # 2. Atualização do jogo (sem nada por enquanto) e Desenhar na tela
+
     tela.fill((255, 255, 255)) #preenchendo tela com a cor branca
+
+    #Desenhando um quadrado
+    pg.draw.rect(tela, VERMELHO, (pos_x, pos_y, 100, 100))
+    pg.draw.circle(tela, AZUL, (200, 200), 40)
+
+    #Desenhando a imagem
+    tela.blit(personagem_img, (pos_personagem_x, pos_personagem_y))
     
     # Atualização da tela para exibir possíveis mudanças
     pg.display.flip()
+
+pg.quit()
      
