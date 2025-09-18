32e9d076cbff178767c99ad083135fa0e2daf316
Kevin <kevinsleao@gmail.com>
2025-09-18 15:29:23 -0300

    camera incompleto

--- main.py
+++ main.py
@@ -53,7 +53,17 @@ pos_x = 50
 pos_y = 50
 
 #Velocidade do personagem
-velocidade_pers = 0.5
+velocidade_pers = 50
+
+#Variável de controle da tela
+is_fullscreen = False
+
+#posição da camera
+camera_y = pos_personagem_x - largura_J / 2
+camera_x = pos_personagem_y - altura_J / 2
+
+#inicialização do clock
+clock = pg.time.Clock()
 
 #Criando loop de gameplay
 rodando = True
@@ -64,8 +74,25 @@ while rodando:
         # If para fechar a janela quando o "X" da janela for pressionado
         if evento.type == pg.QUIT:
             rodando = False #Mudando o valor da variável de controle para acabar com o loop
+
+        #Redimensionamento da tela
+        if evento.type == pg.KEYDOWN:
+            if evento.key == pg.K_F11:
+                is_fullscreen = not is_fullscreen
+                if is_fullscreen:
+                    tela = pg.display.set_mode((0,0), pg.FULLSCREEN)
+                else:
+                    tela = pg.display.set_mode((largura_J, altura_J))
+
+        if evento.type == pg.VIDEORESIZE:
+            largura_J = evento.w
+            altura_J = evento.h
+
+            tela = pg.display.set_mode((largura_J, altura_J))
             
     # 2. Atualização do jogo (sem nada por enquanto) e Desenhar na tela
+
+    dt = clock.tick(60) / 100
 
     #Detecção de input (pressionado) :P 
     key = pg.key.get_pressed()
@@ -94,8 +121,11 @@ while rodando:
         deslocamento_y /= normalizador
 
     #Atualização da localização
-    pos_personagem_x += deslocamento_x * velocidade_pers
-    pos_personagem_y += deslocamento_y * velocidade_pers
+    pos_personagem_x += deslocamento_x * velocidade_pers * dt
+    pos_personagem_y += deslocamento_y * velocidade_pers * dt
+
+    camera_y = pos_personagem_x - largura_J / 2
+    camera_x = pos_personagem_y - altura_J / 2
 
     #Virar para mira de mouse
     #Pegar posição do mouse
@@ -115,7 +145,8 @@ while rodando:
     imagem_rotacionada = pg.transform.rotate(personagem_img, angulo)
 
     #Obtem um novo retângulo e obtem o cento dele
-    rect_rotacionado = imagem_rotacionada.get_rect(center=(centro_personagem_x, centro_personagem_y))
+    # rect_rotacionado = imagem_rotacionada.get_rect(center=(centro_personagem_x, centro_personagem_y))
+    rect_rotacionado = imagem_rotacionada.get_rect(center=(largura_J / 2, altura_J / 2))
 
     tela.fill((255, 255, 255)) #preenchendo tela com a cor branca
 
@@ -131,4 +162,3 @@ while rodando:
     pg.display.flip()
 
 pg.quit()
-     
